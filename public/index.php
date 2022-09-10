b476cda4f99649cf57d77338458efc2149158279
bansi-2001 <chothanibansari47@gmail.com>
2022-09-10 10:27:43 +0530

    initioal commit

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+echo "adjksfjkhf";
+// $username="root";//change username 
+// $password=""; //change password
+// $host="localhost";
+// $db_name="status"; //change databasename
+
+// $conn=mysqli_connect($host,$username,$password,$db_name);
+
+// if(!$conn)
+// {
+// 	echo json_encode("Connection Failed");
+// }
+
+// $query="select * from status";
+
+// $q = mysqli_query($conn,$query);
+
+// $res =array();
+// while($r=mysqli_fetch_assoc($q)){
+// 	$res[] = $r;
+// }
+// echo json_encode($res);
+// $conn->close();
+?>
